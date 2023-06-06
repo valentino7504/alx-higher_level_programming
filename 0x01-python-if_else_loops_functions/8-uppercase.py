2144fa06eab01e81203d0081cbf8afcbf25fd166
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-06 16:13:01 +0100

    Solved task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(old_string):
+    new_string = ""
+    for letter in old_string:
+        if 97 <= ord(letter) <= 122:
+            new_string += chr(ord(letter) - 32)
+        else:
+            new_string += letter
+    print(new_string)
+    return new_string
