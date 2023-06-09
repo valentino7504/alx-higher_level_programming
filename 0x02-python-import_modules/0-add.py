df4eb8a0c9ae876a71746a9876d4f31147a1db44
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-09 12:45:06 +0100

    Fixed task 0 issues

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ != "__main__":
-    exit()
-import add_0 as maths_lib
-a = 1
-b = 2
-c = maths_lib.add(a, b)
-print("{} + {} = {}".format(a, b, c))
+    import add_0 as addition
+    a = 1
+    b = 2
+    c = addition.add(a, b)
+    print("{} + {} = {}".format(a, b, c))
