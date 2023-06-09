2e7722d794db069eebd06abbada25e65fa3df6d7
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-09 13:05:27 +0100

    Fixed punctuation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,12 @@
 if __name__ == "__main__":
     import sys
     argument_str = "argument"
+    punctuation = ":"
     args_no = len(sys.argv)
     if args_no != 2:
         argument_str = "arguments"
-    print(f"{args_no - 1} {argument_str}.")
+    if args_no == 1:
+        punctuation = "."
+    print(f"{args_no - 1} {argument_str}{punctuation}")
     for i in range(1, args_no):
         print(f"{i}: {sys.argv[i]}")
