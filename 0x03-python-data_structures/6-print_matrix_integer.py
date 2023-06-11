8585274e3a1da59680670c8b6cbe45cafa8fb738
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-11 11:06:26 +0100

    Fixed empty matrix error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+        return
     for row in matrix:
         for i in range(len(row)):
             end = " " if i != len(row) - 1 else "\n"
