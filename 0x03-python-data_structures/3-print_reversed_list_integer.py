4367c9bfeee4ce23376190b56bc1ab849b66b819
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-11 11:07:39 +0100

    Undid empty list print

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
     if length == 0:
-        print()
         return
     for num in range(length):
         print("{:d}".format(my_list[length - num - 1]))
