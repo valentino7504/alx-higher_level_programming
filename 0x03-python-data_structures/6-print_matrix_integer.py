fa0e8707e2579b82baa9bab7aa13641e512f80be
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-11 11:04:58 +0100

    Fixed task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == [[]]:
-        print()
-        return
     for row in matrix:
         for i in range(len(row)):
             end = " " if i != len(row) - 1 else "\n"
