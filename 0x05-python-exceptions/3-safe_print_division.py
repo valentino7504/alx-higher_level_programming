9fbac224d1ddc30639ef09e2ad3053b187aa8ef5
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-26 16:45:29 +0100

    Solved task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a/b
+    except (ValueError, TypeError, ZeroDivisionError):
+        result = None
+    finally:
+        print("Inside result: ", end="")
+        print("{}".format(result))
+    return result
