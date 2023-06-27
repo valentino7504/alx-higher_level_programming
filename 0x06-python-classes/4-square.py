383ca202f7f84abc71c106f9a83fb7ab89c64770
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-27 19:06:48 +0100

    Solved task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A square with getter and setter for size"""
+
+
+class Square:
+    """a square class with getter and setter"""
+    def __init__(self, size=0) -> None:
+        self.size = size
+
+    @property
+    def size(self):
+        """returns the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """sets the size of the square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.size ** 2
