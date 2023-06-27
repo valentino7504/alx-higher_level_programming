c000547f5012672337761457219163c41fede85e
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-27 18:20:38 +0100

    Corrected init signature

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """a square class with area method"""
-    def __init__(self, size) -> None:
+    def __init__(self, size=0) -> None:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size <= 0:
