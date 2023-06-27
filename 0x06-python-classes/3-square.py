fdab0156a2835eca05601080342bedc778a50e58
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-27 18:21:59 +0100

    Corrected value check

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,10 +7,11 @@ class Square:
     def __init__(self, size=0) -> None:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size <= 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
     def area(self):
+        """Calculates area of the circle"""
         return (self.__size ** 2)
