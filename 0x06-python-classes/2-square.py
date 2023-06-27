72f8cd8830fb2a9aaf6c816427e4fe1c378170b6
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-27 18:14:51 +0100

    Solved task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module creates a square class
+with some validation for the size"""
+
+
+class Square:
+    """A square class with validated size"""
+    def __init__(self, size=0) -> None:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
