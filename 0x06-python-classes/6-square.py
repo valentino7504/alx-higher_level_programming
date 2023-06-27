ecdf76eb876f860989ce9b7f6efb3820f97ec694
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-27 19:37:15 +0100

    Solved task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""A square with getter and setter for size
+Area and print methods also included"""
+
+
+class Square:
+    """a square class with getter, setter, printer and area method"""
+    def __init__(self, size=0, position=(0, 0)) -> None:
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """returns the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """sets the size of the square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """returns the area"""
+        return self.size ** 2
+
+    def raise_pos_error(self):
+        raise TypeError("position must be a tuple of 2 positive integers")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if len(value) != 2:
+            self.raise_pos_error()
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            self.raise_pos_error()
+        if value[0] < 0 or value[1] < 0:
+            self.raise_pos_error()
+        self.__position = value
+
+    def my_print(self):
+        """prints the circle to stdout with position offset"""
+        if self.size == 0:
+            print()
+            return
+        for _ in range(self.position[1]):
+            print()
+        for _ in range(self.size):
+            print(" " * self.position[0], end="")
+            print("#"*self.size)
