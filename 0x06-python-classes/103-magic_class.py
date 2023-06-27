1abc351636b59e461c6a151b29e4e705a68a119e
Edwin Ade <edwinmbonyjr@gmail.com>
2023-06-27 22:12:19 +0100

    Renamed class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """Creates a circle class"""
 import math
+PI = math.pi
 
 
-class Circle:
+class MagicClass:
     """This is a circle class with radius, area and
     circumference"""
     def __init__(self, radius=0) -> None:
-        self.__radius = 0
         if not isinstance(radius, int) and not isinstance(radius, float):
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
         """gets the area of the circle"""
-        return math.pi * (self.__radius ** 2)
+        return PI * (self.__radius ** 2)
 
     def circumference(self):
         """gets the circumference of the circle"""
-        return 2 * self.__radius * math.pi
+        return 2 * self.__radius * PI
