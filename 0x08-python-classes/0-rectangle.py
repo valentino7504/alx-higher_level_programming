6f0359bb9a8b4a5c44ae13237694cd91a28b4954
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-03 12:25:25 +0100

    Solved task 0

---
+++ 0x08-python-classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+Definition of an empty rectangle class for use in
+0x08 - more classes and objects
+
+"""
+
+
+class Rectangle:
+    """
+    An empty rectangle class
+    """
+    pass
---
+++ 0x08-python-classes/README.md
@@ -0,0 +1,3 @@
+# 0x08 More Classes and Objects
+
+More on classes and objects in Python
