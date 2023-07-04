fd039a1a812bcc852577a404c6c0c9bfd2360223
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-04 10:34:33 +0100

    Initial commit

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Provides a function to copy a list
+"""
+
+
+def copy_list(my_list):
+    """
+    This function copies a list
+    """
+    return my_list[:]
---
+++ 0x09-python-everything_is_object/README.md
@@ -0,0 +1,3 @@
+# 0x09 Everything is object
+
+Solution to tasks in 0x09 Everything is object in ALX
