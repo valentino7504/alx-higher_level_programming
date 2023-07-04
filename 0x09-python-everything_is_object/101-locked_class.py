1830babc78c54d7e3f199722b00daaa0e524d35c
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-04 12:15:45 +0100

    Fixed 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,13 +9,4 @@ class LockedClass:
     My implementation of a locked class where you can only set
     first_name
     """
-
-    def __init__(self) -> None:
-        pass
-
-    def __setattr__(self, __name: str, __value) -> None:
-        error_msg = f"'LockedClass' object has no attribute '{__name}'"
-        if not hasattr(self, "first_name") and __name == "first_name":
-            super().__setattr__("first_name", __value)
-        else:
-            raise AttributeError(error_msg)
+    __slots__ = ["first_name"]
