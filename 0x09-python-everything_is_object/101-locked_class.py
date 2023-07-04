7a83c67a0812c85d4d9a53e25877b0b005e2d67d
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-04 11:05:59 +0100

    Created locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+creating a locked class in python
+"""
+
+
+from typing import Any
+
+
+class LockedClass:
+    """
+    My implementation of a locked class where you can only set
+    first_name
+    """
+    def __init__(self) -> None:
+        pass
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        error_msg = f"'LockedClass' object has no attribute '{__name}'"
+        if __name == "first_name":
+            super().__setattr__("first_name", __value)
+        else:
+            raise AttributeError(error_msg)
