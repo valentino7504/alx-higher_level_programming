bfff4fa20cf6f750edf9cd8a6da518b0868fa482
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-10 15:46:32 +0100

    Solved task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+This module checks if an object is an instance of a class
+or is inherited from it
+
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    This checks if obj is an instance of a_class or inherits
+    """
+    return isinstance(obj, a_class)
