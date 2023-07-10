6ce3880d7da48547b58534271c743a11aa5f5448
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-10 15:47:42 +0100

    Renamed function

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,7 +7,7 @@ or is inherited from it
 """
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
     This checks if obj is an instance of a_class or inherits
     """
