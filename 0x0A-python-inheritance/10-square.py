ca1421fbc4f75b7ce285d200421da651bdffe5df
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-10 20:42:38 +0100

    Solved task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a square class based on
+the rectangle
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    This is a custom square class
+    """
+    def __init__(self, size) -> None:
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
