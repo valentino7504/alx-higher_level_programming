aa8f8e5c1f8f1322ad83563fcb7a3c63716f2064
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-10 20:34:02 +0100

    Solved task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+
+this module defines a rectangle class for use
+in 0x0A
+
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    This is  a rectangle class with various methods
+    """
+    def __init__(self, width, height) -> None:
+        super().__init__()
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
