574ec5aecc019d169fd2f3981275e946e6da3d7c
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-11 12:43:01 +0100

    Solved task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+
+This file is used to save json format of an object to a file
+
+
+'''
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    saves the json string to a file
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(json.dumps(my_obj))
