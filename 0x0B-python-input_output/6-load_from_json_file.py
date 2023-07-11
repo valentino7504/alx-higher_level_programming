0bea78208c73445d50a24f18948867f49302f75b
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-11 12:45:16 +0100

    Solved task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+
+This module loads a json string as an object from a file
+
+'''
+
+
+import json
+
+
+def load_from_json(filename):
+    """
+    reads a file and obtains a json object from it
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.loads(file.read())
