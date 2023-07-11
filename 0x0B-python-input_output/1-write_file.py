5c942b97e45288f374019bbedaca98fab55e3898
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-11 12:02:19 +0100

    Added docs for function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,6 +7,9 @@ This module writes to a file
 
 
 def write_file(filename="", text=""):
+    """
+    This function writes content to a file
+    """
     with open(filename, "w", encoding="utf-8") as file:
         written = file.write(text)
     return written
