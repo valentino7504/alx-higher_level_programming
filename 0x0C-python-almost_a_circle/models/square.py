679dcffc3d2466e9fdf3d62cd0e3b43aa91228c1
Edwin Ade <edwinmbonyjr@gmail.com>
2023-07-17 15:52:43 +0100

    Fixed size attribute

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,8 +32,12 @@ class Square(Rectangle):
         """
         sets the value of size
         """
-        self.width = value
-        self.height = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
 
     def __str__(self) -> str:
         """
