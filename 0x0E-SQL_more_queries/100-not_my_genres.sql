8d25a1b65965ddc58562e552695b3d985320169b
Edwin Ade <edwinmbonyjr@gmail.com>
2023-08-16 22:21:54 +0100

    Refactored extra 1

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@ SELECT tv_genres.name AS name
 FROM tv_genres
 WHERE name NOT IN
 (
-SELECT tv_genres.name AS name
+SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id=tv_show_genres.genre_id
