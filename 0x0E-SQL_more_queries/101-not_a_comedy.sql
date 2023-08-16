4a113f6faeeae614292df695389b2895a230cb16
Edwin Ade <edwinmbonyjr@gmail.com>
2023-08-16 22:24:56 +0100

    Solved extra task 2

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,14 @@
+-- retrieves all shows that are not comedies
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+(
+SELECT title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name="Comedy"
+)
+ORDER BY title ASC;
