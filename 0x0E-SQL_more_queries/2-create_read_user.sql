0f66a1b0d0a7ebc47a3e7f2b0ef3e3bc846270a7
Edwin Ade <edwinmbonyjr@gmail.com>
2023-08-16 18:03:07 +0100

    Solved task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Creates a user in mysql serve with all privileges
+-- if the user already exists do not create
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
