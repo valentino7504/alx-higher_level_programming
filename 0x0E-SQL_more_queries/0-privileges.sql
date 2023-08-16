e20b3cf81ce394fc953fcb904d9ee9aca0780565
Edwin Ade <edwinmbonyjr@gmail.com>
2023-08-16 17:16:52 +0100

    Fixed task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- checks privileges of a mysql user
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
