c179922c6db74d1b54b25ab71a35e7630908bc22
Edwin Ade <edwinmbonyjr@gmail.com>
2023-08-16 21:44:27 +0100

    Refactored 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,13 +1,9 @@
 -- lists all the genres of dexter
 SELECT tv_genres.name AS name
 FROM tv_genres
-WHERE tv_genres.id IN (
-    SELECT genre_id 
-    FROM tv_show_genres 
-    WHERE tv_show_genres.show_id=(
-        SELECT id 
-        FROM tv_shows 
-        WHERE tv_shows.title="Dexter"
-    )
-)
+INNER JOIN tv_show_genres
+ON tv_genres.id=tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title="Dexter"
 ORDER BY name ASC;
