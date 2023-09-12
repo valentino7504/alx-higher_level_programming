fa0108c7d633de08dce7c3a13e3913ab6c3695c7
Edwin Ade <edwinmbonyjr@gmail.com>
2023-09-12 22:29:01 +0100

    Fix: Semistandard style violation

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 const argv = process.argv;
-for (let i = 2; i < 4; i++){
+for (let i = 2; i < 4; i++) {
   fs.readFile(argv[i], (err, file) => {
     if (err) throw err;
     fs.appendFile(argv[4], file.toString(), (err) => {
       if (err) throw err;
     });
   });
-};
+}
