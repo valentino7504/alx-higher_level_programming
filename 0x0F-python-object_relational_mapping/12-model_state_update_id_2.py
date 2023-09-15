1ab628278152a5307a97d5575405189d78243f82
Edwin Ade <edwinmbonyjr@gmail.com>
2023-09-15 14:31:09 +0100

    Add: Solution to task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+
+Updates the state whose id is 2
+
+'''
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}" +
+                           f"@localhost:3306/{argv[3]}", pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).where(State.id==2).first()
+    state.name = "New Mexico"
+    session.add(state)
+    session.commit()
