cddf762b8fd6499d1eaa41aa9b5a317cb32f4baf
Edwin Ade <edwinmbonyjr@gmail.com>
2023-09-15 12:35:05 +0100

    Fix: Case checking

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=f"{argv[1]}",
                          passwd=f"{argv[2]}", db=f"{argv[3]}", port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
     data = cur.fetchall()
     for row in data:
         print(row)
