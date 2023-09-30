081e3e56fc6790a10616526c89c173d21c4d4d80
Edwin Ade <edwinmbonyjr@gmail.com>
2023-09-30 01:00:23 +0100

    Add: Solution to task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+
+makes a POST request and gets json response
+
+"""
+
+
+import requests
+import sys
+
+
+argv = sys.argv
+q = "" if len(argv) == 1 else argv[1]
+params = {"q": q}
+url = "http://0.0.0.0:5000/search_user"
+response = requests.post(url, data=params)
+try:
+    data = response.json()
+except ValueError:
+    print("Not a valid JSON")
+else:
+    if data == {}:
+        print("No result")
+    else:
+        print(f"[{data.get('id')}] {data.get('name')}")
