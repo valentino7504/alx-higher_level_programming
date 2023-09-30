5c0ca31b45b75046cf6a732a4cc1f92654b73951
Edwin Ade <edwinmbonyjr@gmail.com>
2023-09-30 01:01:36 +0100

    Fix: Fixed task 8 error with main guard

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,17 +10,18 @@ import requests
 import sys
 
 
-argv = sys.argv
-q = "" if len(argv) == 1 else argv[1]
-params = {"q": q}
-url = "http://0.0.0.0:5000/search_user"
-response = requests.post(url, data=params)
-try:
-    data = response.json()
-except ValueError:
-    print("Not a valid JSON")
-else:
-    if data == {}:
-        print("No result")
+if __name__ == "__main__":
+    argv = sys.argv
+    q = "" if len(argv) == 1 else argv[1]
+    params = {"q": q}
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data=params)
+    try:
+        data = response.json()
+    except ValueError:
+        print("Not a valid JSON")
     else:
-        print(f"[{data.get('id')}] {data.get('name')}")
+        if data == {}:
+            print("No result")
+        else:
+            print(f"[{data.get('id')}] {data.get('name')}")
