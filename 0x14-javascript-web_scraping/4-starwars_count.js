cc244782104ab7aef278357910ffb839fee8e321
Edwin Ade <edwinmbonyjr@gmail.com>
2024-01-03 20:51:53 +0000

    Fix: Made a correction to task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // gets the number of starwars movies Wedge Antilles is in
 const request = require('request');
-const characterID = 18;
-const charactersURL = 'https://swapi-api.alx-tools.com/api/people/';
 const url = process.argv[2];
 request(url, (error, response, body) => {
   let wedgeMovies = 0;
@@ -11,7 +9,7 @@ request(url, (error, response, body) => {
   } else {
     const movies = JSON.parse(body).results;
     movies.forEach(movie => {
-      if (movie.characters.includes(charactersURL + characterID + '/')) {
+      if (movie.characters.some(person => person.includes(`18`))) {
         wedgeMovies++;
       }
     });
