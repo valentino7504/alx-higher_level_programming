d245d799f2ae07f1399985b3262ffd03419343f1
Edwin Ade <edwinmbonyjr@gmail.com>
2024-01-03 22:08:23 +0000

    Fix: reverted extra task 2

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,20 +2,12 @@
 // script that prints all the characters in a star wars
 // movie
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (error) console.log(error);
   else {
-    const films = JSON.parse(body).results;
-    let film;
-    for (let i = 0; i < films.length; i++) {
-      if (films[i].episode_id.toString() === movieId) {
-        film = films[i];
-        break;
-      }
-    }
-    film.characters.forEach(characterURL => {
+    const filmDetails = JSON.parse(body);
+    filmDetails.characters.forEach(characterURL => {
       request(characterURL, (error, response, body) => {
         if (error) console.log(error);
         else {
